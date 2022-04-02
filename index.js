6a32c846f8f049d5a98a4b948af9227b4e4d6683
eztatik <jromero29knights@gmail.com>
2022-04-02 14:10:53 -0400

    Added node.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+// require('./startup/logging')();
+// require('./startup/routes')(app);
+// require('./startup/db')();
+// require('./startup/config')();
+// require('./startup/validation')();
+// require('./startup/prod')(app);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+   
+  });
+  router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/css/styles.css'));
+   
+  });
+  
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
+
+
+
+
+    
