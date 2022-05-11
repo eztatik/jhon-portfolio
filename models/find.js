da55f155b5f85d58c99165c3e757e71e47c801dd
eztatik <jromero29knights@gmail.com>
2022-05-11 00:52:23 -0400

    Finder working correctly now

--- models/find.js
+++ models/find.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {Author, validate} = require('./author');
 
-
+//Currently working correctly
 router.get('/', async (req,res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     const author = await Author.find().sort( 'name' );
     res.send(author);
   });
-  router.get('/:id', async (req,res) => {
-    const author = await Author.findById(req.params.id);
-    if (!author) res.status(404).send('author with the given id was not found');
+  router.post('/view', async (req,res) => {
+    const author = await Author.findById(req.body.id);
+    if (!author) return res.status(404).send('author with the given id was not found');
     res.send(author);
   });
 
